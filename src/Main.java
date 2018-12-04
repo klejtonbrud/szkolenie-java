ea8210e0447359698298c3e3eb398a20d320233d
klejtonbrud <adipe97@gmail.com>
2018-12-04 15:45:54 +0100

    zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
-import java.util.Random;
-
 public class Main
 {
 
     public static void main(String[] args)
     {
-        Random random = new Random();
-
-        int x = random.nextInt(5)+1;
-        System.out.println(x);
+        for(int i=0;i<args.length;i++)
+        {
+            System.out.println(args[i]);
+        }
     }
 }
