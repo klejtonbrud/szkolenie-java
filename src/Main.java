39a784e1915f232f5dc99f99c9cbf5b2fb96d490
klejtonbrud <adipe97@gmail.com>
2018-12-05 13:30:12 +0100

    zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main
 {
+    public static boolean isLeapYear(int year)
+    {
+        if(year % 4 == 0)
+        {
+            if(year % 100 != 0)
+            {
+                return true;
+            }
+            else if(year % 400 == 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
 
     public static void main(String[] args)
     {
